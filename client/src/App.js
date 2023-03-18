885ed41e2a4bbb801b9b5c3514c68f02e34d3aba
The Brahmnic Boy <thebrahmnicboy@gmail.com>
2023-03-18 13:37:53 +0530

    feat: Add routes and setup project
    Co-authored-by: Shreyashka  Patel <ShreyashkaPatel@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import LandingPage from './components/LandingPage';
+import Choose from './components/Choose';
+import Spaces from './components/Spaces';
+import Grievances from './components/Grievances';
+import Chat from './components/Chat';
+import Reports from './components/Reports';
+import MainAppbar from './components/MainAppbar';
 
-function App() {
+export default function App() {
+
+  const navigate = useNavigate();
+
+  const localTheme = window.localStorage.getItem('hackathonAppTheme');
+
+  const [mode, setMode] = useState(localTheme ? localTheme : 'light');
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+
+    typography: {
+      fontFamily: "'Open Sans', sans-serif",
+    },
+  });
+
+  const themeChange = () => {
+    const updatedTheme = mode === 'light' ? 'dark' : 'light';
+    window.localStorage.setItem('hackathonAppTheme', updatedTheme);
+    setMode(updatedTheme);
+  };
+
+  const isSignedIn = true;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+
+      <Routes>
+        {/*  */}
+
+        <Route
+          path='/'
+          element={<LandingPage themeChange={themeChange} mode={mode} />}
+        />
+
+        <Route
+          path='/choose'
+          element={<Choose themeChange={themeChange} mode={mode} />}
+        />
+
+        <Route
+          path='/spaces'
+          element={<Spaces themeChange={themeChange} mode={mode} />}
+        />
+
+        <Route
+          path='/spaces/:id'
+          element={<Spaces themeChange={themeChange} mode={mode} />}
+        />
+
+        <Route
+          path='/grievances'
+          element={<Grievances themeChange={themeChange} mode={mode} />}
+        />
+
+        <Route
+          path='/grievances/:id'
+          element={<Grievances themeChange={themeChange} mode={mode} />}
+        />
+
+        <Route
+          path='/chat'
+          element={<Chat themeChange={themeChange} mode={mode} />}
+        />
+
+        <Route
+          path='/chat/:id'
+          element={<Chat themeChange={themeChange} mode={mode} />}
+        />
+
+        <Route
+          path='/reports'
+          element={<Reports themeChange={themeChange} mode={mode} />}
+        />
+
+      </Routes>
+
+      {isSignedIn ? (
+        <MainAppbar
+          {...{
+            themeChange,
+            mode,
+          }}
+        />
+      ) : null}
+
+    </ThemeProvider>
   );
 }
-
-export default App;
