e40f1a70c9fe405c8dc3474b172feca9dcfc43e4
The Brahmnic Boy <thebrahmnicboy@gmail.com>
2023-03-19 01:00:05 +0530

    fix: Change CreateGrievance Path in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import LandingPage from './components/LandingPage';
 import Choose from './components/Choose/Choose';
 import Spaces from './components/Spaces';
 import Grievances from './components/Grievances/Grievances.js';
-import CreateGrievance from './components/CreateGrievance';
+import CreateGrievance from './components/Grievances/CreateGrievance';
 import Chat from './components/Chat';
 import Reports from './components/Reports';
 import MainAppbar from './components/MainAppbar';
