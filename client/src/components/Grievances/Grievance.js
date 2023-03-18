951316708be97a550e317220ba782fa608e649b6
The Brahmnic Boy <thebrahmnicboy@gmail.com>
2023-03-19 04:25:41 +0530

    feat: Complete Thread Header of Grievances

--- client/src/components/Grievances/Grievance.js
+++ client/src/components/Grievances/Grievance.js
@@ -4,15 +4,14 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 // Material UI
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
 import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
-import Avatar from '@mui/material/Avatar';
 
 // Components
 import BarredPage from '../BarredPage';
 import ConsistentButton from '../ConsistentButton';
+import GrievanceThreadParent from './GrievanceThreadParent';
 
 // Actions
 import { startLoadingAction, stopLoadingAction } from '../../actions/actions';
@@ -29,6 +28,7 @@ export default function Grievance({ mode }) {
     const dispatch = useDispatch();
 
     const [grievance, setGrievance] = useState({});
+
     const routeInfo = window.location.pathname.split('/')[1] + '/' + window.location.pathname.split('/')[2];
     const id = window.location.pathname.split('/')[2];
 
@@ -72,71 +72,10 @@ export default function Grievance({ mode }) {
 
                 <Divider orientation='horizontal' sx={{ margin: '2vh' }} />
 
-                <Box
-                    sx={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'flex-start',
-                        p: 2,
-                        m: 2,
+                {/* Component for Main Greivance */}
 
-                        backgroundColor: mode === 'light' ? lMode2 : dMode2,
-                        borderRadius: '20px',
-                    }}
-                >
-                    <Typography
-                        component='div'
-                        sx={{
-                            color: mode === 'light' ? lMode1 : dMode1,
-                            font: '500 2.2rem Poppins, sans-serif',
-                        }}
-                    >
-                        {grievance.title}
-                    </Typography>
+                <GrievanceThreadParent mode={mode} grievance={grievance} />
 
-                    {/* Poster Details */}
-                    {grievance?.createdBy && (
-                        <Box
-                            sx={{
-                                display: 'flex',
-                                flexDirection: 'row',
-                                justifyContent: 'flex-start',
-                                alignItems: 'center',
-                                marginTop: '0.5rem',
-                            }}
-                        >
-                            <Avatar
-                                alt={grievance.createdBy.name}
-                                src={grievance.createdBy.avatarURL}
-                                sx={{
-                                    marginRight: '0.5rem',
-                                    backgroundColor: 'white',
-                                }}
-                            />
-
-                            <Divider orientation='vertical' sx={{
-                                height: '2rem',
-                                mr: 1,
-                                backgroundColor: mode === 'light' ? lMode3 : dMode3,
-                            }} />
-
-                            <Typography
-                                variant='subtitle1'
-                                sx={{ color: mode === 'light' ? lMode6 : dMode6 }}
-                            >
-                                {grievance.createdBy.name}
-                            </Typography>
-                        </Box>
-                    )}
-
-                    {/* Description */}
-                    <Typography
-                        variant='body1'
-                        sx={{ color: mode === 'light' ? lMode3 : dMode3, mt: '1rem' }}
-                    >
-                        {grievance.description}
-                    </Typography>
-                </Box>
             </Box>
         </BarredPage>
     );
